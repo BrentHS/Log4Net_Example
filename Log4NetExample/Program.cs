662ff91c5488e3f21c93b7426b425c2ab297a29e
Brent Schavee <BrentHS@BrentHS.com>
2013-05-17 15:17:29 -0500

    Copied existing log 4 net code over in order to get up to Git Hub.

---
+++ Log4NetExample/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Log4NetExample
+{
+	using System;
+	using System.IO;
+	using System.Reflection;
+	using log4net;
+	using log4net.Config;
+
+	class Program
+	{
+		private static readonly log4net.ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Starting...");
+
+			//log4net
+			//XmlConfigurator.ConfigureAndWatch(new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log4net.config")));
+			//XmlConfigurator.ConfigureAndWatch(new FileInfo("log4net.config"));
+			XmlConfigurator.Configure();
+
+			_log.Info("I'm logging something!");
+
+			var testClass = new TestClass();
+			testClass.ImAnotherClass();
+
+			Console.WriteLine("Done.");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Log4NetExample/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Log4NetExample
+{
+	using System.Reflection;
+
+	using log4net;
+
+	class TestClass
+	{
+		private static readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+		public void ImAnotherClass()
+		{
+			_log.Debug("Some more logging!");
+		}
+	}
+}
